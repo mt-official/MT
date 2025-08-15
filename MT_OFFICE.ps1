f7bc2ea978803c3154ccc67cda029fca6fde7032
MT <mohameedthoufiq004@gmail.com>
2025-08-15 22:23:00 +0530

    MT_OFFICE.ps1

--- MT_OFFICE.ps1
+++ MT_OFFICE.ps1
@@ -1,11 +1,12 @@
 # Wrapper for HWID_Activation.cmd
-$cmdUrl  = "https://raw.githubusercontent.com/mt-official/MT/refs/heads/main/Ohook_Activation_AIO_office.bat"
-$cmdPath = "$env:TEMP\Ohook_Activation_AIO_office.bat"
+$cmdUrl  = "https://raw.githubusercontent.com/mt-official/MT/refs/heads/main/MT_office.cmd"
+$cmdPath = "$env:TEMP\MT_office.cmd"
 
-Write-Host "`n📥 Downloading Ohook_Activation_AIO_office.bat ..." -ForegroundColor Cyan
+Write-Host "`n📥 Downloading MT_Office.cmd ..." -ForegroundColor Cyan
 Invoke-WebRequest -Uri $cmdUrl -OutFile $cmdPath -UseBasicParsing
 
 Write-Host "🚀 Running script..." -ForegroundColor Yellow
 Start-Process "cmd.exe" -ArgumentList "/C `"$cmdPath`"" -NoNewWindow -Wait
 
 Write-Host "`n✅ Done running Ohook_Activation_AIO_office.bat" -ForegroundColor Green
+
