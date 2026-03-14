e78f7c9a3a992d975fa1bbf78fc4e42679f0d684
MT <mohameedthoufiq004@gmail.com>
2026-03-14 16:08:31 +0530

    Add files via upload

---
+++ cwe.ps1
@@ -0,0 +1,10 @@
+$cmdUrl = "https://raw.githubusercontent.com/mt-official/MT/main/cwe.cmd"
+$cmdPath = "$env:TEMP\cwe.cmd"
+
+Write-Host "Downloading..." -ForegroundColor Cyan
+
+Invoke-WebRequest $cmdUrl -OutFile $cmdPath
+
+Write-Host "Running..." -ForegroundColor Yellow
+
+Start-Process cmd.exe -ArgumentList "/c `"$cmdPath`"" -Verb RunAs
